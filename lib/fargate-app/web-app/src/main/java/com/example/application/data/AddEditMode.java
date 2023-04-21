e1a6e0e6d1f8d874325760fa086ab9317eb09e9b
Gabe Merton <gmerton@amazon.com>
2023-04-21 06:07:45 +0000

    Swap step 2

--- lib/fargate-app/web-app/src/main/java/com/example/application/data/AddEditMode.java
+++
@@ -1,8 +0,0 @@
-// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
-// SPDX-License-Identifier: MIT-0
-
-package com.example.application.data;
-
-public enum AddEditMode {
-	ADD, EDIT;
-}
